6894b3bd2b9a01c331e38b552db4b41545930dd6
DanielM02-app <luismontalvang343@outlook.com>
2025-03-30 13:26:38 -0600

    First

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+export default function DashboardPage() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="bg-white p-8 rounded-lg shadow-lg w-96 text-center">
+          <h1 className="text-2xl font-semibold text-green-600 mb-4">
+            ✨ Bienvenido ✨
+          </h1>
+          <p className="text-gray-700">
+            Has logrado ingresar al sistema de manera exitosa.
+          </p>
+        </div>
+      </div>
+    );
+  }
